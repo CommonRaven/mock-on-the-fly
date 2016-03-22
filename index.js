b75220a25f334c46edf8076578c4f4a1ad35b0ad
Ido Schachter <ido.schachter@ironsrc.com>
2016-03-22 11:19:38 +0200

    better args handling + and date format

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const _ = require('lodash');
 const mockOTF = require('./lib');
 
-var model = ['$$5', {
+var model = ['$$1', {
     'id': '$$randomString',
     'dates': ['$$2', {
-        'date': '$$date'
+        'date': '$$date(2012,1,1)'
     }]
 }];
 
-var seed = 'abcdefg';
+var seed = 'n54g8gg57pe1e0e6208m3h9bcpb23bd8lekmgm8o9jd';
 var mock = mockOTF.parse(model, seed);
 console.log(JSON.stringify(mock, null, 2));
